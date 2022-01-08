5680bad092fa15f2d2129a0baa5127de65a744fc
Shreyasi Patil <shreyasipatil404@gmail.com>
2022-01-09 02:05:31 +0530

    main task wiring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { GlobalHotKeys} from "react-hotkeys";
+import confetti from "canvas-confetti";
+import React from 'react';
 
 function App() {
+
+    const keyMap = {
+    KEY_WORD: 'c h a n g w o r k s',
+    };
+
+    const handlers = {
+    KEY_WORD: burstConfetti
+    };
+
+    function burstConfetti(){
+
+    return confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 }
+    });
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Type changworks</h1>
+        <GlobalHotKeys handlers={handlers} className="node" keyMap={keyMap}/>
     </div>
   );
 }
